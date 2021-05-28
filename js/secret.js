4b47bc483b57e9b2f26e826daee9b7e7e863c8f0
Vin Lacey <vin.lacey@aol.com>
2021-05-28 22:01:22 +0100

    switched to an addEventListener to reveal secret div instead of onclick

--- js/secret.js
+++ js/secret.js
@@ -1,9 +1,11 @@
 // the secret sauce
-let secretId = document.getElementById('secret');
+const secretId = document.getElementById('secret')
 
 secretId.style.display = 'none';          // Hide on page load
 
+let imageMe = document.getElementById("image-me")
+
 // reveal secret when my photo is clicked.
-function clickPhoto() {
-  secretId.style.display = 'block';
-}
+
+imageMe.addEventListener("click",
+  () => secretId.style.display = 'block') // click the photo for an easter egg
