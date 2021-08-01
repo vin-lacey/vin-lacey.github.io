e463a855bab39597f0e6e34968bc330eb9079a5c
Vin Lacey <vin.lacey@aol.com>
2021-08-01 14:42:56 +0100

    add new fact button and new projects section

--- scripts/fact.js
+++ scripts/fact.js
@@ -1,8 +1,15 @@
 // Gives me a random fact on my homepage
 // thanks to the useless facts API: https://uselessfacts.jsph.pl
 
-let factOutput = document.getElementById('random-fact')
+const factOutput = document.getElementById('random-fact')
+const factButton = document.getElementById('fact-button')
 
-fetch('https://uselessfacts.jsph.pl/random.json?language=en')
-  .then(response => response.json())
-  .then(data => factOutput.textContent = data.text);
+const factFetch = () => {
+  fetch('https://uselessfacts.jsph.pl/random.json?language=en')
+    .then(response => response.json())
+    .then(data => factOutput.textContent = data.text);
+}
+
+factFetch()
+
+factButton.addEventListener( 'click', () => factFetch() )
