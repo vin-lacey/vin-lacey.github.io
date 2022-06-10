ada63926b34ec51cfad669dfdab04288388f0131
Vin Lacey <vin.lacey@aol.com>
2022-06-10 03:59:48 +0100

    add contact.php

---
+++ contact.php
@@ -0,0 +1,24 @@
+<?php
+echo "Hello World!";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact me | Vin Lacey</title>
+</head>
+
+<body>
+    <div class="container">
+        <div class="form">
+            <form>
+
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
