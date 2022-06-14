3e971c4897f5a002c4672eb439ac5f06196cb04f
Vin Lacey <vin.lacey@aol.com>
2022-06-14 12:59:23 +0100

    add php form logic test

--- contact.php
+++ contact.php
@@ -12,27 +12,25 @@
 <body>
     <div class="container">
         <div class="form">
-            <form>
-                <div class="form-container">
-                    <form id="contact" action="" method="post">
-                        <h3>Contact me</h3>
-                        <div class="form-flex">
-                            <div class="form-input-field">
-                                <input placeholder="Your name" type="text" tabindex="1" required autofocus>
-                            </div>
-                            <div class="form-input-field">
-                                <input placeholder="Your Email Address" type="email" tabindex="2" required>
-                            </div>
-                            <div class="form-input-field">
-                                <textarea placeholder="Type your Message Here...." tabindex="3" required></textarea>
-                            </div>
-                            <div class="form-button">
-                                <button name="submit" type="submit" id="contact-submit">Submit</button>
-                            </div>
+            <div class="form-container">
+                <form id="contact" action="formsubmit.php" method="post">
+                    <h3>Contact me</h3>
+                    <div class="form-flex">
+                        <div class="form-input-field">
+                            <input placeholder="Your name" type="text" tabindex="1" id="name" required autofocus>
                         </div>
-                    </form>
-                </div>
-            </form>
+                        <div class="form-input-field">
+                            <input placeholder="Your Email Address" type="email" id="email" tabindex="2" required>
+                        </div>
+                        <div class="form-input-field">
+                            <textarea placeholder="Type your Message Here...." id="message" tabindex="3" required></textarea>
+                        </div>
+                        <div class="form-button">
+                            <button name="submit" type="submit" id="contact-submit">Submit</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </body>
---
+++ formsubmit.php
@@ -0,0 +1,11 @@
+<html>
+
+<body>
+
+    Welcome <?php echo $_POST["name"]; ?><br>
+    Your email address is: <?php echo $_POST["email"]; ?>
+    Your message is: <?php echo $_POST["message"]; ?>
+
+</body>
+
+</html>
