c654f0c8c059e793af341e4a243c197f6fc83755
Vin Lacey <vin.lacey@aol.com>
2022-06-14 10:05:38 +0100

    add form divs

--- contact.php
+++ contact.php
@@ -1,6 +1,3 @@
-<?php
-echo "Hello World!";
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,6 +5,7 @@ echo "Hello World!";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/form.css">
     <title>Contact me | Vin Lacey</title>
 </head>
 
@@ -15,23 +13,24 @@ echo "Hello World!";
     <div class="container">
         <div class="form">
             <form>
-                <div class="container">
+                <div class="form-container">
                     <form id="contact" action="" method="post">
                         <h3>Contact me</h3>
-                        <fieldset>
-                            <input placeholder="Your name" type="text" tabindex="1" required autofocus>
-                        </fieldset>
-                        <fieldset>
-                            <input placeholder="Your Email Address" type="email" tabindex="2" required>
-                        </fieldset>
-                        <fieldset>
-                            <textarea placeholder="Type your Message Here...." tabindex="5" required></textarea>
-                        </fieldset>
-                        <fieldset>
-                            <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
-                        </fieldset>
+                        <div class="form-flex">
+                            <div class="form-input-field">
+                                <input placeholder="Your name" type="text" tabindex="1" required autofocus>
+                            </div>
+                            <div class="form-input-field">
+                                <input placeholder="Your Email Address" type="email" tabindex="2" required>
+                            </div>
+                            <div class="form-input-field">
+                                <textarea placeholder="Type your Message Here...." tabindex="3" required></textarea>
+                            </div>
+                            <div class="form-button">
+                                <button name="submit" type="submit" id="contact-submit">Submit</button>
+                            </div>
+                        </div>
                     </form>
-
                 </div>
             </form>
         </div>
